ad4d2ed71061d082e51d911a6a71b770a0180d8a
BIDMUS Folami <folami.bidmus@gmail.com>
2022-08-31 21:54:10 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+/* alert(); */
+
+// change nav bar style on scroll
+window.addEventListener('scroll', () => {
+    const nav = document.querySelector('nav');
+    nav.classList.toggle('window-scroll', window.scrollY > 10);
+});
+
+//show/hide faq answers
+const faqs = document.querySelectorAll('.faq');
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        // change icon
+        const icon = faq.querySelector('.faq__icon i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = 'uil uil-minus';
+        } else {
+            icon.className = 'uil uil-plus';
+        }
+    });
+});
+
+// show/hide nav menu on tablets and mobile
+const menu = document.querySelector('.nav__menu');
+const menuBtn = document.querySelector('#open-menu-btn');
+const closeBtn = document.querySelector('#close-menu-btn');
+
+menuBtn.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    closeBtn.style.display = 'inline-block';
+    menuBtn.style.display = 'none';
+});
+
+// close nav menu on click
+const closeNav = () => {
+    menu.style.display = 'none';
+    closeBtn.style.display = 'none';
+    menuBtn.style.display = 'inline-block';
+}
+
+closeBtn.addEventListener('click', closeNav);
